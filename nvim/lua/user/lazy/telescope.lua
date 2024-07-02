950e2f03357d61e8622bd31352c127f65e100758
EvgenySusaev <susaevjack@yandex.ru>
2024-07-02 07:44:40 +0300

    Nvim plugin configs were added.

--- nvim/lua/user/lazy/telescope.lua
+++ nvim/lua/user/lazy/telescope.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+    tag = "0.1.8",
 
     dependencies = {
         "nvim-lua/plenary.nvim"
@@ -10,20 +10,10 @@ return {
     config = function()
         require('telescope').setup({})
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-        vim.keymap.set('n', '<leader>pws', function()
-            local word = vim.fn.expand("<cword>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>pWs', function()
-            local word = vim.fn.expand("<cWORD>")
-            builtin.grep_string({ search = word })
-        end)
-        vim.keymap.set('n', '<leader>ps', function()
-            builtin.grep_string({ search = vim.fn.input("Grep > ") })
-        end)
-        vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+    	local builtin = require('telescope.builtin')
+	    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+	    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+	    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+	    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
     end
 }

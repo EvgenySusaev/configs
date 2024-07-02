ea424f46b0db072bb819cb077bc1e62938bdb3c6
EvgenySusaev <susaevjack@yandex.ru>
2024-07-02 21:23:09 +0300

    Nvim config was changed.

--- nvim/lua/user/remap.lua
+++ nvim/lua/user/remap.lua
@@ -2,3 +2,11 @@ vim.g.mapleader = ' '
 
 vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
 vim.keymap.set('i', 'jk', '<Esc>', { noremap = true, silent = true })
+
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window ", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })

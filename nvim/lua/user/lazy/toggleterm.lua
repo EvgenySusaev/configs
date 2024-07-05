112dcb6a8365d5969168bdbfdd066a223e864808
EvgenySusaev <susaevjack@yandex.ru>
2024-07-05 08:52:24 +0300

    Nvim config was changed.

--- nvim/lua/user/lazy/toggleterm.lua
+++ nvim/lua/user/lazy/toggleterm.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         require("toggleterm").setup {
           size = 20,
-          open_mapping = [[<leader>t]],
+          open_mapping = [[<F7>]],
           hide_numbers = true,
           shade_filetypes = {},
           shade_terminals = true,
@@ -13,8 +13,5 @@ return {
           persist_size = true,
           direction = 'float',
         }
-        vim.api.nvim_set_keymap('n', '<F7>', ':ToggleTerm<CR>', {noremap = true, silent = true})
-        vim.api.nvim_set_keymap('t', '<F7>', '<C-\\><C-n>:ToggleTerm<CR>', {noremap = true, silent = true})
-
     end
 }

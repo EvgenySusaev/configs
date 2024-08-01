0b65f29a666d51d2c1272b4c0129aee47ea10bab
Eugene Susaev <susaevjack@yandex.ru>
2024-08-01 22:32:00 +0300

    Nvim config was changed.

--- nvim/lua/user/set.lua
+++ nvim/lua/user/set.lua
@@ -1,5 +1,11 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor = {
+    "n-v-c:block-Cursor",
+    "i-ci:ver25-CursorInsert",
+    "r-cr:hor20-CursorReplace",
+    "sm:block-blinkwait175-blinkoff150-blinkon175"
+}
 
+vim.opt.hidden = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
@@ -9,6 +15,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
+vim.opt.cursorline = true
 
 vim.opt.wrap = false
 
@@ -17,7 +24,7 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
